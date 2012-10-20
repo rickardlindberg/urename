b5d9cda098bc3bd70a307578289b431e1393487b
Rickard Lindberg <ricli85@gmail.com>
2012-10-20 14:47:11 +0200

    fix semantic error and python import subs

--- src/plugin_python.py
+++ src/plugin_python.py
@@ -6,8 +6,8 @@ class PythonPlugin(object):
     def tasks(self, src, dest):
         all_files = find(".")
         py_files = [x for x in all_files if x.endswith(".py")]
-        old = ""
-        new = ""
+        old = os.path.basename(src.path)[:-3]
+        new = os.path.basename(dest.path)[:-3]
         return [
             SubstituteTask(py_files, "import %s" % old, "import %s" % new),
             SubstituteTask(all_files, src.path, dest.path)
--- src/plugins.py
+++ src/plugins.py
@@ -1,11 +1,11 @@
 plugins = []
 
 
-def register_plugin(self, plugin):
+def register_plugin(plugin):
     plugins.append(plugin)
 
 
-def get_plugin(self, src, dest):
+def get_plugin(src, dest):
     for plugin in plugins:
         if plugin.use(src, dest):
             return plugin
