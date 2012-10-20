a3d1cbcd762bea6a0ba3b8e08ac8164e37a47a95
Rickard Lindberg <ricli85@gmail.com>
2012-10-20 14:32:20 +0200

    duplicate file

---
+++ src/plugin_generic.py
@@ -0,0 +1,43 @@
+class GeneralPlugin(object):
+
+    def __init__(self, src, dest):
+        self.src = src
+        self.dest = dest
+
+    def tasks(self):
+        all_files = find(".")
+        return [SubstituteTask(all_files, self.src.path, self.dest.path)]
+
+
+class PythonPlugin(object):
+
+    def __init__(self, src, dest):
+        self.src = src
+        self.dest = dest
+
+    def tasks(self):
+        all_files = find(".")
+        py_files = [x for x in all_files if x.endswith(".py")]
+        old = ""
+        new = ""
+        return [
+            SubstituteTask(py_files, "import %s" % old, "import %s" % new),
+            SubstituteTask(all_files, self.src.path, self.dest.path)
+        ]
+
+
+def find(path):
+    all_files = []
+    for (root, dirs, files) in os.walk(path):
+        all_files += filter(include, [os.path.join(root, x) for x in files])
+    return all_files
+
+
+def include(filepath):
+    if ".git/" in filepath:
+        return False
+    if ".swp" in filepath:
+        return False
+    if os.path.basename(filepath) == "tags":
+        return False
+    return True
