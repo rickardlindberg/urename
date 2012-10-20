df09c48e5fce403bb228cd762c10d6c4487c9821
Rickard Lindberg <ricli85@gmail.com>
2012-10-20 16:02:24 +0200

    ignore more files

--- src/filesystem.py
+++ src/filesystem.py
@@ -12,6 +12,23 @@ def include(filepath):
         return False
     if filepath.endswith(".pyc"):
         return False
+    if filepath.endswith(".class"):
+        return False
     if os.path.basename(filepath) == "tags":
         return False
+    if is_binary(filepath):
+        return False
     return True
+
+
+def is_binary(filename):
+    """
+    Return true if the given filename appears to be binary.
+    File is considered to be binary if it contains a NULL byte.
+    FIXME: This approach incorrectly reports UTF-16 as binary.
+    """
+    with open(filename, 'rb') as f:
+        for block in f:
+            if '\0' in block:
+                return True
+    return False
